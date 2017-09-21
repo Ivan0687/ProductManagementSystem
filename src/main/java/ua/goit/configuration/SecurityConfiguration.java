7b545bf9f7ebef221bd96e591ead5ee02ec7a3b3
Vancho <ivpavboyko@gmail.com>
2017-09-21 15:59:20 +0300

    heroku

--- src/main/java/ua/goit/configuration/SecurityConfiguration.java
+++ src/main/java/ua/goit/configuration/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 // указываем URL логаута
                 .logoutUrl("/logout")
                 // указываем URL при удачном логауте
-//                .logoutSuccessUrl("/login?logout")
+                .logoutSuccessUrl("/")
                 // делаем не валидной текущую сессию
                 .invalidateHttpSession(true)
             .and()
